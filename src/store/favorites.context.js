d93ae7596b4a0ee112a933079e4930fa78d0ce30
kiwoonjung <kiwoon0627@gmail.com>
2023-03-02 13:14:45 -0800

    add updating state based on previous state

--- src/store/favorites.context.js
+++ src/store/favorites.context.js
@@ -8,6 +8,24 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
+  function addFavoriteHandler(favoriteBucketList) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteBucketList);
+    });
+  }
+
+  function removeFavoriteHandler(bucketListId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter(
+        (bucketList) => bucketList.id !== bucketListId
+      );
+    });
+  }
+
+  function itemIsFavoriteHandler(bucketListId) {
+    return userFavorites.some((bucketList) => bucketList.id === bucketListId);
+  }
+
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
